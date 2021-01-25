594046e98f0f77c27b3d1581f2aa58c9b5b1be66
siixmy <77422902+siixmy@users.noreply.github.com>
2021-01-25 13:40:12 +0800

    Update HelloController.java
    t

--- springboot/src/main/java/com/example/springboot/HelloController.java
+++ springboot/src/main/java/com/example/springboot/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello Sping Boot!aaaahhjjkkll";
+        return "Hello Sping Boot!aaaahhjjkklltt";
     }
 }
