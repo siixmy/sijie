4149bccc83e9d0a847657d5036326d235aa36ce1
natsu <1948492207@qq.com>
2021-01-25 12:40:02 +0800

    提交

--- springboot/src/main/java/com/example/springboot/HelloController.java
+++ springboot/src/main/java/com/example/springboot/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello Sping Boot!aaaaddcc";
+        return "Hello Sping Boot!aaaahhjj";
     }
 }
