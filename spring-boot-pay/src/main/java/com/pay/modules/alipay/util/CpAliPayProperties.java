ae3ea590214cf61dbbb507db061d2df5842af3af
natsu <1948492207@qq.com>
2021-01-27 10:22:36 +0800

    提交

--- spring-boot-pay/src/main/java/com/pay/modules/alipay/util/CpAliPayProperties.java
+++ spring-boot-pay/src/main/java/com/pay/modules/alipay/util/CpAliPayProperties.java
@@ -14,4 +14,68 @@ public class CpAliPayProperties {
     private String aliPayPublicKey;
     private String notifyUrl;
     private String encryptKey;
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public String getGatewayHost() {
+        return gatewayHost;
+    }
+
+    public void setGatewayHost(String gatewayHost) {
+        this.gatewayHost = gatewayHost;
+    }
+
+    public String getSignType() {
+        return signType;
+    }
+
+    public void setSignType(String signType) {
+        this.signType = signType;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getMerchantPrivateKey() {
+        return merchantPrivateKey;
+    }
+
+    public void setMerchantPrivateKey(String merchantPrivateKey) {
+        this.merchantPrivateKey = merchantPrivateKey;
+    }
+
+    public String getAliPayPublicKey() {
+        return aliPayPublicKey;
+    }
+
+    public void setAliPayPublicKey(String aliPayPublicKey) {
+        this.aliPayPublicKey = aliPayPublicKey;
+    }
+
+    public String getNotifyUrl() {
+        return notifyUrl;
+    }
+
+    public void setNotifyUrl(String notifyUrl) {
+        this.notifyUrl = notifyUrl;
+    }
+
+    public String getEncryptKey() {
+        return encryptKey;
+    }
+
+    public void setEncryptKey(String encryptKey) {
+        this.encryptKey = encryptKey;
+    }
 }
